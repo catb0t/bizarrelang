b74f6cd846e2bdef4bda06eb2f37cbca32fe212c
slingerproductions@gmail.com <slingerproductions@gmail.com@02412efd-48c1-fc76-c19e-27ffe866f541>
2012-11-12 10:25:22 +0000

    Two new units, yay.

---
+++ trunk/compiler.pas
@@ -0,0 +1,13 @@
+unit compiler;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
---
+++ trunk/interpreter.pas
@@ -0,0 +1,13 @@
+unit interpreter;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
